00f6c8a1e41d0d7e02e4905091cfd9c69726c18d
Nguyễn Nhật <nhatnvgcs16457@fpt.edu.vn>
2020-03-01 18:26:27 +0700

    Using useDispatch and useSelector to mapStateToProps reducer -- ProductsContainer

--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,20 +1,29 @@
 import React, {useState, useEffect} from 'react';
 import ProductList from './../components/productPage/list/ProductList';
-import {connect} from 'react-redux';
+import {connect, useDispatch, useSelector} from 'react-redux';
 import * as actions from './../actions/index';
 import ProductItem from './../components/productPage/list/ProductItem';
 const ProductsContainer = ({getProduct, dispatchDataByApi,match}) => {
+  const dispatch = useDispatch();
+  // const pro = useSelector(state => state.productReducer)
   const [products, setProducts] = useState ([]);
+  // 
+  const pro = useSelector(state => state.productReducer)
   useEffect (() => {
-    dispatchDataByApi ();
-  }, []);
+    dispatch(actions.getProAPI())
+    // dispatchDataByApi ();
+    // setProducts(pro)
+    // console.log(pro);
+  }, [dispatch]);
   useEffect (
     () => {
-      setProducts (getProduct);
+      // setProducts (getProduct);
+      setProducts(pro )
+      console.log(products);
     },
-    [getProduct]
+    [pro]
   );
-
+  console.log(products);
   const mapArrDataToItem = arr => {
     var rs = null;
     if (arr.length > 0) {
@@ -29,14 +38,14 @@ const ProductsContainer = ({getProduct, dispatchDataByApi,match}) => {
 };
 const mapStateToPropsReducer = state => {
   return {
-    getProduct: state.productReducer,
+    // getProduct: state.productReducer,
   };
 };
 const dispatchActionReducer = (dispatch, props) => {
   return {
-    dispatchDataByApi: () => {
-      dispatch (actions.getProAPI ());
-    },
+    // dispatchDataByApi: () => {
+    //   dispatch (actions.getProAPI ());
+    // },
   };
 };
 export default connect (mapStateToPropsReducer, dispatchActionReducer) (
